99e129cdeb5c95a4deabca0040c8c7f7f996131f
Hamza <hamzayousuf175@gmail.com>
2021-03-07 16:57:23 +0500

    change App in in andriod and added assets

---
+++ src/utils/assets.js
@@ -0,0 +1,14 @@
+const folderPath = `../assets/images/`;
+
+export const images = {
+  authBackground: require(`${folderPath}BG.png`),
+  mainLogo: require(`${folderPath}mainLogo.png`),
+  onBoardingBackGround: require(`${folderPath}onBoardingBackGround.png`),
+  onBoarding1: require(`${folderPath}onBoarding1.png`),
+  onBoarding2: require(`${folderPath}onBoarding2.png`),
+  onBoarding3: require(`${folderPath}onBoarding3.png`),
+  selectLanguage: require(`${folderPath}selectLanguage.png`),
+  locationPermision: require(`${folderPath}locationPermision.png`),
+};
+
+export const icon = {};
