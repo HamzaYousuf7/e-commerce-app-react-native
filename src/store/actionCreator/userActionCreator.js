61608bb06a449aa4af5d8a34bc5f4ce224d477b9
Hamza <hamzayousuf175@gmail.com>
2021-04-11 20:15:10 +0500

    comment example api code

--- src/store/actionCreator/userActionCreator.js
+++ src/store/actionCreator/userActionCreator.js
@@ -5,11 +5,14 @@ import axios from '../../utils/axios';
 import EndPoints from '../../constant/EndPoints';
 
 export const userRegister = (userData) => {
-  // TODO exmaple code remove it
-  console.log('[OUT SIDE RETURN ]', userData);
-  return async (dispatch, getState) => {
-    console.log('[INSDE RETURN] ');
-    const response = await axios.get(EndPoints.TEST);
-    console.log('fetchCoins -> response', response.data);
+  return {
+    type: USER_REGISTER,
   };
+  // TODO exmaple code remove it
+  // console.log('[OUT SIDE RETURN ]', userData);
+  // return async (dispatch, getState) => {
+  //   console.log('[INSDE RETURN] ');
+  //   const response = await axios.get(EndPoints.TEST);
+  //   console.log('fetchCoins -> response', response.data);
+  // };
 };
