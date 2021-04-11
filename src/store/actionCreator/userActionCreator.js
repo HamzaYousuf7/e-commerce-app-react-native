390ebb9d4900f19765761ea713a2b157023c3c7b
Hamza <hamzayousuf175@gmail.com>
2021-04-11 20:11:23 +0500

    added axios and its setup

--- src/store/actionCreator/userActionCreator.js
+++ src/store/actionCreator/userActionCreator.js
@@ -1,14 +1,15 @@
 import {USER_REGISTER} from '../actionType/userActionType';
-import axios from 'axios';
+
+// API imp
+import axios from '../../utils/axios';
+import EndPoints from '../../constant/EndPoints';
 
 export const userRegister = (userData) => {
   // TODO exmaple code remove it
   console.log('[OUT SIDE RETURN ]', userData);
   return async (dispatch, getState) => {
     console.log('[INSDE RETURN] ');
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts',
-    );
+    const response = await axios.get(EndPoints.TEST);
     console.log('fetchCoins -> response', response.data);
   };
 };
